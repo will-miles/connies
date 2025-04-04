1b5b6822a926625f521c7d96a7a6281950582b49
will-miles <willmiles@live.co.uk>
2025-04-03 21:38:14 -0600

    filter by distance now working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import boulderJson from '../data/boulder_crags.json';
 import sportJson from '../data/sport_crags.json';
 import tradJson from '../data/trad_crags.json';
 import winterJson from '../data/winter_crags.json';
-import { computeDistanceBetween } from 'spherical-geometry-js';
 import React, { Component } from 'react';
+import getDistanceFromLatLonInKm from '../utils.ts';
 
 const cragObj: { [index: string]: any } = {
   boulder: boulderJson,
@@ -27,8 +27,8 @@ class Home extends Component {
     }[];
   } = {
     style: '',
-    location: '',
-    range: 50000,
+    location: '53.52574940018496, -1.919685834247175',
+    range: 50,
     crags: [],
   };
   render() {
@@ -84,6 +84,7 @@ class Home extends Component {
           <li className='flex flex-col animate-pulse m-4 h-64 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 shadow-lg'>
             <div>
               <h2>{crag.name}</h2>
+              <h2>{crag.distance}</h2>
             </div>
           </li>
         ))}
@@ -120,9 +121,37 @@ class Home extends Component {
 
     // example location: 53.52574940018496, -1.919685834247175
 
+    const searchLatLong = searchObj.location.split(', ');
+    console.log(searchLatLong);
+    const searchLat = parseFloat(searchLatLong[0]);
+    const searchLong = parseFloat(searchLatLong[1]);
+
+    const filteredCragArray = cragObj[searchObj.style].filter(
+      (crag: {
+        aspect: string;
+        rockType: string;
+        long: string;
+        lat: string;
+        name: string;
+        numStaredClimbs: string;
+        distance: number;
+      }) => {
+        const distance = getDistanceFromLatLonInKm(
+          searchLat,
+          searchLong,
+          parseFloat(crag.lat),
+          parseFloat(crag.long)
+        );
+        crag.distance = distance;
+        return distance < searchObj.range ? true : false;
+      }
+    );
+
+    console.log(filteredCragArray);
+
     if (cragObj[searchObj.style]) {
       console.log(searchObj);
-      this.setState({ crags: cragObj[searchObj.style] });
+      this.setState({ crags: filteredCragArray });
     }
   };
 }
