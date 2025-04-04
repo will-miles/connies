36ddef95ae545b41f79c9f4ab7485715a8633568
will-miles <willmiles@live.co.uk>
2025-04-03 19:08:25 -0600

    rendering list of crags of matching climbing style

--- src/data/acquisition/table_data_extraction.ts
+++ src/data/acquisition/table_data_extraction.ts
@@ -1,4 +1,22 @@
 import { winter, trad, sport, boulder } from './raw_tables.js';
+import { writeFile } from 'fs/promises';
+
+type importhtmls = {
+  html1: string;
+  html2: string;
+  html3: string;
+};
+
+type imports = {
+  winter: importhtmls;
+  sport: importhtmls;
+  trad: importhtmls;
+  boulder: importhtmls;
+};
+
+const imports: imports = { winter, trad, sport, boulder };
+
+const Arr = ['winter', 'trad', 'sport', 'boulder'];
 
 type crag = {
   aspect: String;
@@ -9,7 +27,14 @@ type crag = {
   numStaredClimbs: String;
 };
 
-const regexs = {
+const regexs: {
+  aspect: RegExp;
+  rockType: RegExp;
+  long: RegExp;
+  lat: RegExp;
+  name: RegExp;
+  numStaredClimbs: RegExp;
+} = {
   aspect: /Faces:<\/strong> <small>(.*)<\/small><br>/g,
   rockType: /Rock:<\/strong><br><small>(.*)<\/small>/g,
   long: /data-osx="([-\.\d]+)"/g,
@@ -21,37 +46,43 @@ const regexs = {
 
 const regexKeys = Object.keys(regexs);
 
-const trArray1: string[] = boulder.html1.split('<tr ');
-const trArray2: string[] = boulder.html2.split('<tr ');
-const trArray3: string[] = boulder.html3.split('<tr ');
+Arr.forEach((style) => {
+  const trArray1: string[] =
+    imports[style as keyof imports].html1.split('<tr ');
+  const trArray2: string[] =
+    imports[style as keyof imports].html2.split('<tr ');
+  const trArray3: string[] =
+    imports[style as keyof imports].html3.split('<tr ');
 
-const trArray = [...trArray1, ...trArray2, ...trArray3];
+  const trArray = [...trArray1, ...trArray2, ...trArray3];
 
-const crags: crag[] = [];
+  const crags: crag[] = [];
 
-trArray.forEach((tableRow) => {
-  if (tableRow) {
-    const crag: crag = {
-      aspect: '',
-      rockType: '',
-      long: '',
-      lat: '',
-      name: '',
-      numStaredClimbs: '',
-    };
-    regexKeys.forEach((regexKey) => {
-      const matchArray = [...tableRow.matchAll(regexs[regexKey])][0];
-      if (matchArray) {
-        crag[regexKey] = matchArray[1];
+  trArray.forEach((tableRow) => {
+    if (tableRow) {
+      const crag: crag = {
+        aspect: '',
+        rockType: '',
+        long: '',
+        lat: '',
+        name: '',
+        numStaredClimbs: '',
+      };
+      regexKeys.forEach((regexKey) => {
+        const matchArray = [
+          ...tableRow.matchAll(regexs[regexKey as keyof typeof regexs]),
+        ][0];
+        if (matchArray) {
+          crag[regexKey as keyof typeof regexs] = matchArray[1];
+        }
+      });
+      if (crag.name) {
+        crags.push(crag);
       }
-    });
-    if (crag.name) {
-      crags.push(crag);
     }
-  }
+  });
+
+  const cragsFiltered = [...new Set(crags)];
+
+  writeFile(`../${style}_crags.json`, JSON.stringify(cragsFiltered));
 });
-
-const cragsFiltered = [...new Set(crags)];
-
-import { writeFile } from 'fs/promises';
-await writeFile('../boulder_crags.json', JSON.stringify(cragsFiltered));
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,130 @@
-const Home = () => {
-  return (
-    <div>
-      <h1 className='justify-center m-auto text-center'>
-        process.env.REACT_APP_ENV_VARIABLE ={' '}
-        {process.env.REACT_APP_ENV_VARIABLE}
-      </h1>
-    </div>
-  );
+import boulderJson from '../data/boulder_crags.json';
+import sportJson from '../data/sport_crags.json';
+import tradJson from '../data/trad_crags.json';
+import winterJson from '../data/winter_crags.json';
+import { computeDistanceBetween } from 'spherical-geometry-js';
+import React, { Component } from 'react';
+
+const cragObj: { [index: string]: any } = {
+  boulder: boulderJson,
+  sport: sportJson,
+  trad: tradJson,
+  winter: winterJson,
 };
+
+class Home extends Component {
+  state: {
+    style: string;
+    location: string;
+    range: number;
+    crags: {
+      aspect: string;
+      rockType: string;
+      long: string;
+      lat: string;
+      name: string;
+      numStaredClimbs: string;
+    }[];
+  } = {
+    style: '',
+    location: '',
+    range: 50000,
+    crags: [],
+  };
+  render() {
+    return (
+      <div>
+        <h1 className='justify-center m-auto text-center'>Connies</h1>
+        <form className=''>
+          <label>Choose a climbing style:</label>
+          <select id='styles' name='styles'>
+            <option value='boulder'>Boulder</option>
+            <option value='sport'>Sport</option>
+            <option value='trad'>Trad</option>
+            <option value='winter'>Winter</option>
+          </select>
+          <label>What is your location?</label>
+          <input
+            id='location'
+            onChange={this.handleLocationChange}
+            value={this.state.location}
+          ></input>
+          <label>What range?</label>
+          <input
+            id='range'
+            onChange={this.handleRangeChange}
+            value={this.state.range}
+          ></input>
+          <button
+            onClick={(e: any) => {
+              this.handleFromSubmit(e);
+            }}
+          >
+            Search
+          </button>
+        </form>
+        <this.makeCragList />
+      </div>
+    );
+  }
+
+  makeCragList = () => {
+    const crags: {
+      aspect: string;
+      rockType: string;
+      long: string;
+      lat: string;
+      name: string;
+      numStaredClimbs: string;
+    }[] = this.state.crags;
+    console.log(crags);
+    return (
+      <ul className='flex flex-wrap justify-center'>
+        {crags.map((crag) => (
+          <li className='flex flex-col animate-pulse m-4 h-64 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 shadow-lg'>
+            <div>
+              <h2>{crag.name}</h2>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  handleLocationChange = () => {
+    const location = document.getElementById('location') as HTMLSelectElement;
+    this.setState({ location: location.value });
+  };
+
+  handleRangeChange = () => {
+    const range = document.getElementById('range') as HTMLSelectElement;
+    this.setState({ range: range.value });
+  };
+
+  handleFromSubmit = (e: any) => {
+    e.preventDefault();
+    const styles: any = document.getElementById('styles') as HTMLSelectElement;
+
+    enum Direction {
+      boulder,
+      sport,
+      trad,
+      winter,
+    }
+
+    const searchObj: { style: Direction; location: string; range: number } = {
+      style: styles.value,
+      location: this.state.location,
+      range: this.state.range,
+    };
+
+    // example location: 53.52574940018496, -1.919685834247175
+
+    if (cragObj[searchObj.style]) {
+      console.log(searchObj);
+      this.setState({ crags: cragObj[searchObj.style] });
+    }
+  };
+}
 
 export default Home;
